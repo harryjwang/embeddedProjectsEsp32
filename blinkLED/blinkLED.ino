0815348ed5b14676c160ff82145609e15a9653e8
Harry Wang <hjwang@uwaterloo.ca>
2024-06-07 23:53:39 -0400

    new projet and minor changes to first project

--- blinkLED/blinkLED.ino
+++ blinkLED/blinkLED.ino
@@ -1,4 +1,6 @@
+/* SAME CODE AS THE BLINKING GPIO PIN 2 LED */
 #define PIN_LED 2
+int count = 0;
 // the setup function runs once when you press reset or power the board
 void setup() {
  // initialize digital pin LED_BUILTIN as an output.
@@ -6,8 +8,14 @@ void setup() {
 }
 // the loop function runs over and over again forever
 void loop() {
+  while (count < 10){
   digitalWrite(PIN_LED, HIGH); // turn the LED on (HIGH is the voltage level)
-  delay(5000); // wait for a second
+  delay(1000); // wait for a second
   digitalWrite(PIN_LED, LOW); // turn the LED off by making the voltage LOW
-  delay(1000);
+  delay(1000); 
+  count++;
+  if (count == 10){
+    break;
+  }
+  } 
 }
